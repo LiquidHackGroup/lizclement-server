3ae27a27b707c69e051f2019f5c06badb9075cc7
allistermugaisi <allistermugaisi@gmail.com>
2022-10-31 12:49:19 +0300

    added database env uri

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 // Connect to MongoDB
 mongoose
-	.connect(process.env.MONGO_URI_LOCAL, {
+	.connect(process.env.MONGO_URI, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
